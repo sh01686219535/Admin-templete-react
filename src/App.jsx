7c280eb161d72ab77fc67703a39bb0a91cff334f
Md.Shariful islam <sharif.net888@gmail.com>
2025-03-03 12:11:03 +0600

    left sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 
-import { BrowserRouter, Route, Routes  } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/dashboard';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Header/>
-        <Routes>
-          <Route path="/" exact={true} element={<Dashboard/>} />
-          <Route path="/dashboard" exact={true} element={<Dashboard/>} />
-        </Routes>
+        <Header />
+        <div className="main flex">
+          <div className="sidebarWrapper">
+            <Sidebar />
+          </div>
+          <div className="content">
+            <Routes>
+              <Route path="/" exact={true} element={<Dashboard />} />
+              <Route path="/dashboard" exact={true} element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
       </BrowserRouter>
     </>
   );
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,163 @@
+import Button from '@mui/material/Button';
+import { MdDashboard } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaProductHunt } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa6";
+import { MdMessage } from "react-icons/md";
+import { FaBell } from "react-icons/fa6";
+import { IoSettings } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { IoMdLogOut } from "react-icons/io";
+
+
+const Sidebar = () => {
+    const [activeTab, setActiveTab] = useState(0);
+    const [isToggleSubMenu, setIsToggleSubMenu] = useState(false);
+
+    const isOpenSubmenu = (index) => {
+        setActiveTab(index);
+        setIsToggleSubMenu(!isToggleSubMenu);
+    }
+
+    return (
+        <>
+            <div className="sidebar">
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 0 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(0)}>
+                                <span className='icon'><MdDashboard /></span>
+                                Dashboard
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Button className={`w-full ${activeTab === 1 && isToggleSubMenu === true ? 'active' : ''}`} onClick={() => isOpenSubmenu(1)}>
+                            <span className='icon'><FaProductHunt /></span>
+                            products
+                            <span className='arrow'><IoIosArrowForward /></span>
+                        </Button>
+                        <div className={`submenuwapper ${activeTab === 1 && isToggleSubMenu === true ? 'colapse' : 'colapsed'}`}>
+                            <div className="submenu">
+                                <ul>
+                                    <li><Link to="#">Product List</Link></li>
+                                    <li><Link to="#">Product View</Link></li>
+                                    <li><Link to="#">Product Upload</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 2 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(2)}>
+                                <span className='icon'><FaCartPlus /></span>
+                                orders
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 3 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(3)}>
+                                <span className='icon'><MdMessage /></span>
+                                message
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 4 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(4)}>
+                                <span className='icon'><FaBell /></span>
+                                notifications
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 5 && isToggleSubMenu === true ? 'active' : ''}`} onClick={() => isOpenSubmenu(5)}>
+                                <span className='icon'><IoSettings /></span>
+                                Settings
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 6 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(6)}>
+                                <span className='icon'><MdDashboard /></span>
+                                Dashboard
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 7 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(7)}>
+                                <span className='icon'><FaProductHunt /></span>
+                                products
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 8 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(8)}>
+                                <span className='icon'><FaCartPlus /></span>
+                                orders
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 9 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(9)}>
+                                <span className='icon'><MdMessage /></span>
+                                message
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 10 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(10)}>
+                                <span className='icon'><FaBell /></span>
+                                notifications
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 11 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(11)}>
+                                <span className='icon'><IoSettings /></span>
+                                Settings
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <Button className={`w-full ${activeTab === 12 && isToggleSubMenu === true  ? 'active' : ''}`} onClick={() => isOpenSubmenu(12)}>
+                                <span className='icon'><IoSettings /></span>
+                                Settings
+                                <span className='arrow'><IoIosArrowForward /></span>
+                            </Button>
+                        </Link>
+                    </li>
+                </ul>
+                <div className="logoutWrapper">
+                    <div className="logoutBox">
+                        <Button variant="contained"><IoMdLogOut/>Logout</Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Sidebar;
